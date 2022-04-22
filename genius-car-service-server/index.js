d12406fe7d1ceb332ef4cf129b376a7c76c5b804
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-04-22 17:41:03 +0600

    genius car crd

--- genius-car-service-server/index.js
+++ genius-car-service-server/index.js
@@ -37,6 +37,21 @@ async function run() {
       const service = await serviceCollection.findOne(query)
       res.send(service)
     })
+
+    //post
+    app.post('/services', async (req, res) => {
+      const newService = req.body
+      const result = await serviceCollection.insertOne(newService)
+      res.send(result)
+    })
+
+    //delete
+    app.delete('/services/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
   } finally {
   }
 }
